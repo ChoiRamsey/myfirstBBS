6f38d0149d2c41201d6e0151ccc4888a666f0986
최진우 <choiramsey8@gmail.com>
2023-03-06 23:56:29 +0900

    add: home route handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentsModule } from './comments/comments.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PostsModule, AuthModule, CommentsModule, BookmarksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
